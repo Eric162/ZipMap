05a1d393d37b6d14f0cb5f6547ecff3fcd1279f6
Eric Shirley <shirley_eric@bah.com>
2016-08-30 10:02:11 -0400

    cleanup

--- map_resources/parse_to_mongo.sh
+++ map_resources/parse_to_mongo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Starting MongoDB import:
-mongoimport --db maptest --collection zips --file --type json --file zipcode_poly_docs.json --jsonArray
+mongoimport --db maptest --collection zips --type json --file zipcode_poly_docs.json --jsonArray
 echo Import Complete
